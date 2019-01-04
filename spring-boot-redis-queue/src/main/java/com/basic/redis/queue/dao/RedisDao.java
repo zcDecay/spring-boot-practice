e279bbb94d1d8ec94d686e617f72d57e79668a80
zcDecay <15611700291@163.com>
2019-01-04 18:05:03 +0800

    SpringBoot进阶 --> 优化redis队列

---
+++ spring-boot-redis-queue/src/main/java/com/basic/redis/queue/dao/RedisDao.java
@@ -0,0 +1,31 @@
+package com.basic.redis.queue.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description redis
+ * @Author zcc
+ * @Date 19/01/04
+ */
+@Component
+public class RedisDao {
+
+    @Autowired
+    private StringRedisTemplate template;
+
+    public void setKey(String key, String value) {
+        ValueOperations<String, String> ops = template.opsForValue();
+        //1分钟过期
+        ops.set(key, value, 1, TimeUnit.MINUTES);
+    }
+
+    public String getValue(String key) {
+        ValueOperations<String, String> ops = this.template.opsForValue();
+        return ops.get(key);
+    }
+}
